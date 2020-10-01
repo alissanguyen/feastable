ed4f178d8909e01bf9e2dfcd6762a24f6d245295
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2020-10-01 00:46:44 -0700

    Add flash messages.

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -5,6 +5,11 @@ const Restaurant = mongoose.model("Restaurant");
 
 exports.homePage = (req, res) => {
   console.log(req.name);
+  req.flash("error", "<strong>Error 404 Restaurant not found. Add?</strong>");
+  req.flash(
+    "info",
+    "Sorry there was a problem loading this page. Please retry in about 30 seconds."
+  );
   res.render("homePage"); //render index file.
 };
 
@@ -15,7 +20,11 @@ exports.addRestaurant = (req, res) => {
 exports.createRestaurant = async (req, res) => {
   const restaurant = new Restaurant(req.body);
   await restaurant.save();
-  res.redirect("/");
+  req.flash(
+    "success",
+    `Successfully created ${restaurant.name}. Care to leave a review?`
+  );
+  res.redirect("/add");
 };
 
 exports.restaurantTags = (req, res) => {
