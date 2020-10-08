cf090f3a1752b6c53cfdfc20a9a614633e1aedc4
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2020-10-07 22:47:10 -0700

    Create validators for registering.

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,7 +8,7 @@ exports.registerForm = (req, res) => {
   res.render("register", { title: "Account Register" });
 };
 
-exports.validateRegister = (req, res, next) {
+exports.validateRegister = (req, res, next) => {
     req.sanitizeBody('name');
     req.checkBody('name', 'Please enter your name').notEmpty();
     req.checkBody('email', 'Invalid Email Address, please try again.').isEmail();
@@ -17,5 +17,13 @@ exports.validateRegister = (req, res, next) {
         remove_extension: false,
         gmail_remove_subaddress: false
     })
-    req.checkBody('password',)
+    req.checkBody('password', 'Password cannot be blank!').notEmpty();
+    req.checkBody('password-confirm', 'Confirmed password cannot be blank!').notEmpty();
+    req.checkBody('password-confirm', 'Passwords do not match, please try again.').equals(req.body.password);
+
+    const errors = req.validationErrors();
+    if (error) {
+        req.flash('error', errors.map(err => err.msg));
+        res.render('register', {title: 'Register', body: req.body, flashes: req.flash() });
+    }
 }
