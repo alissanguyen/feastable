e704e190c5396dc220f808bce6e07f6343a8d007
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2020-10-07 22:11:23 -0700

    Create user login page.

--- models/user.js
+++ models/user.js
@@ -5,3 +5,13 @@ const Schema = mongoose.Schema;
 mongoose.Promise = global.Promise
 
 const md5 = require('md5');
+
+const validator = require('validator');
+
+const mongodbErrorHandler = require('mongoose-mongodb-errors');
+const passportLocalMongoose = require('password-local-mongoose');
+
+const userSchema = new Schema({
+
+});
+
